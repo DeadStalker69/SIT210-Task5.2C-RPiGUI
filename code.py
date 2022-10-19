841c90bddc841ed91c42c9b19a0857266d80372d
DeadStalker69 <106471305+DeadStalker69@users.noreply.github.com>
2022-10-19 21:00:21 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,14 +2,14 @@ from tkinter import *
 import tkinter.font
 from gpiozero import LED as Red		    #using gpiozero module. this helps to its functions which are shorter and easier as compared to rpi.GPIO's functions. 
 									    #import LED enables LED functions, like is_lit, off, on etc. 
-from gpiozero import LED as Yellow
+from gpiozero import LED as Blue
 from gpiozero import LED as Green
 import RPi.GPIO						   
 
 RPi.GPIO.setmode(RPi.GPIO.BOARD)	   #using GPIO naming convention
 
 red = Red(14)						    #setting GPIO 14 for red LED
-yellow = Yellow(15)					    #setting GPIO 15 for yellow LED
+blue = Blue(15)					    #setting GPIO 15 for blue LED
 green = Green(18)					    #setting GPIO 18 for green LED
 
 win = Tk()							    #tkinter function to open a new window
@@ -22,22 +22,22 @@ def redToggle():						#defining redToggle command, used in redButton
 		redButton["text"] = "Turn Red LED on"
 										#and change the text visible on the button to Turn Red LED on.		
 	else:
-		yellow.off()					#turn yellow LED off if Red LED is off and button to turn Red LED on is pressed
+		blue.off()					#turn blue LED off if Red LED is off and button to turn Red LED on is pressed
 		green.off()						#turn green LED off if Red LED is off and button to turn Red LED on is pressed
 		red.on()						#turn red LED on if Red LED is off and button to turn Red LED on is pressed
 		redButton["text"] = "Turn Red LED off"
 										#change the text written on the button.
 
 
-def yellowToggle():
-	if yellow.is_lit:
-		yellow.off()
-		yellowButton["text"] = "Turn Yellow LED on"
+def blueToggle():
+	if blue.is_lit:
+		blue.off()
+		blueButton["text"] = "Turn blue LED on"
 	else:
 		red.off()
 		green.off()
-		yellow.on()
-		yellowButton["text"] = "Turn Yellow LED off"
+		blue.on()
+		blueButton["text"] = "Turn blue LED off"
 
 
 def greenToggle():
@@ -46,28 +46,28 @@ def greenToggle():
 		greenButton["text"] = "Turn Green LED on"
 	else:
 		red.off()
-		yellow.off()
+		blue.off()
 		green.on()
 		greenButton["text"] = "Turn Green LED off"
 
 def close():							#defining close command used in exit button and close button
 	red.off()							#turning red LED off
-	yellow.off()						#turning yellow LED off
+	blue.off()						    #turning blue LED off
 	green.off()							#turning green LED off
 	win.destroy()						#closing the GUI window
 
-redButton = Button(win, text = 'Turn Red LED on', font  = Font, command = redToggle, bg = 'red', height = 1, width = 24) 
-										#definng various paramters of a declared button
+redButton = Radiobutton(win, text = 'Turn Red LED on', font  = Font, command = redToggle, width = 124, indicatoron=0) 
+										#definng various paramters of a declared radio button
 redButton.grid(row=0, column=1)
-										#definig the position of the button on the GUI window
+										#definig the position of the radio button on the GUI window
 
-yellowButton = Button(win, text = 'Turn Yellow LED on', font  = Font, command = yellowToggle, bg = 'yellow', height = 1, width = 24)  
-yellowButton.grid(row=1, column=1)
+blueButton = Radiobutton(win, text = 'Turn blue LED on', font  = Font, command = blueToggle, width = 124, indicatoron=0)  
+blueButton.grid(row=1, column=1)
 
-greenButton = Button(win, text = 'Turn Green LED on', font  = Font, command = greenToggle, bg = 'green', height = 1, width = 24)  
+greenButton = Radiobutton(win, text = 'Turn Green LED on', font  = Font, command = greenToggle, width = 124, indicatoron=0)  
 greenButton.grid(row=2, column=1)
 
-exitButton = Button(win, text = 'Exit', font  = Font, command = close, bg = 'blue', height = 1, width = 24)  
+exitButton = Radiobutton(win, text = 'Exit', font  = Font, command = close, width = 124, indicatoron=0)  
 exitButton.grid(row=3, column=1)
 
 win.protocol("WM_DELETE_WINDOW", close)	#giving close function(defined earlier) to exit button
